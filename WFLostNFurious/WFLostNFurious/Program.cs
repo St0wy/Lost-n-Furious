5421e6d30900a97a7531e8a80fe37724beaaf09e
jorge.ltmch <jorge.ltmch@eduge.ch>
2018-02-20 14:11:17 +0100

    re-ajout du projet nettoyé

--- WFLostNFurious/WFLostNFurious/Program.cs
+++ WFLostNFurious/WFLostNFurious/Program.cs
@@ -17,6 +17,7 @@ namespace WFLostNFurious
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //TEST
         }
     }
 }
