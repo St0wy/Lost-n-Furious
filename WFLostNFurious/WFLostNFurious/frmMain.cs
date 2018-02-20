84b72976c22c5340bd0ed565ba85b19ef64b6a71
St0wy <stawer19@gmail.com>
2018-02-20 15:50:57 +0100

    Placement du Labyrinthe Part1

--- WFLostNFurious/WFLostNFurious/frmMain.cs
+++ WFLostNFurious/WFLostNFurious/frmMain.cs
@@ -12,6 +12,7 @@ namespace WFLostNFurious
 {
     public partial class frmMain : Form
     {
+        List<Bloc> labyrinthe = new List<Bloc>();
         public frmMain()
         {
             InitializeComponent();
@@ -19,17 +20,69 @@ namespace WFLostNFurious
 
         private void frmMain_Paint(object sender, PaintEventArgs e)
         {
-            Bloc mur = new Bloc(10, 50);
-            mur.Paint(sender, e);
+            CreationLabyrithe();
+            DoubleBuffered = true;
+        }
 
-            Bloc mur2 = new Bloc(10, 30);
-            mur2.Paint(sender, e);
+        public void CreationLabyrithe()
+        {
+            #region Creation Bordure
+            //Creation Bordure
+            int x = 10, y = 10;
 
-            Bordure bordure1 = new Bordure(10, 10);
-            bordure1.Paint(sender, e);
+            for (int i = 0; i < 17; i++)
+            {
+                creationBordure(x, y);
+                x += 30;
+            }
 
-            Arrivee arrivee1 = new Arrivee(70, 70);
-            arrivee1.Paint(sender, e);
+
+
+            for (int i = 0; i < 17; i++)
+            {
+                creationBordure(x, y);
+                y += 30;
+            }
+
+            for (int i = 0; i < 17; i++)
+            {
+                creationBordure(x, y);
+                x -= 30;
+            }
+
+            for (int i = 0; i < 17; i++)
+            {
+                creationBordure(x, y);
+                y -= 30;
+            }
+            #endregion
+
+            //Creation Arrivee
+            creationArrivee(250, 40);
+            creationArrivee(40, 430);
+            Invalidate();
+            //Bloc mur = new Bloc(10, 50);
+            //mur.Paint(sender, e);
+            //
+            //Bloc mur2 = new Bloc(10, 30);
+            //mur2.Paint(sender, e);
+            //
+            //Arrivee arrivee1 = new Arrivee(70, 70);
+            //arrivee1.Paint(sender, e);
+        }
+
+        public void creationBordure(int x, int y)
+        {
+            var bordure = new Bordure(x, y);
+            labyrinthe.Add(bordure);
+            this.Paint += bordure.Paint;
+        }
+
+        public void creationArrivee(int x, int y)
+        {
+            var arrivee = new Arrivee(x, y);
+            labyrinthe.Add(arrivee);
+            this.Paint += arrivee.Paint;
         }
     }
 }
