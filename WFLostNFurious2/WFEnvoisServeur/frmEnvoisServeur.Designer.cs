4989d1e7ca5fd5306ab2283754d1039de23ec107
St0wy <stawer1911@gmail.com>
2018-05-30 13:48:28 +0200

    fix de l'application serveur

--- WFLostNFurious2/WFEnvoisServeur/frmEnvoisServeur.Designer.cs
+++ WFLostNFurious2/WFEnvoisServeur/frmEnvoisServeur.Designer.cs
@@ -47,6 +47,7 @@
             this.btnSend.TabIndex = 0;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
             // 
             // tbxIp
             // 
